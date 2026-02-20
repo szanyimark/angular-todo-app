32df13622d1146cab800b9ae5356bc187c567379
Szanyi Márk <szanyimark04@gmail.com>
2026-02-20 14:22:33 +0100

    FE: providers in app.config

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,11 +1,24 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideApollo } from 'apollo-angular';
+import { HttpLink, InMemoryCache } from '@apollo/client/core';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withComponentInputBinding())
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(),
+    provideApollo(() => {
+      const httpLink = new HttpLink({
+        uri: 'http://localhost:4000',
+      });
+      return {
+        link: httpLink,
+        cache: new InMemoryCache(),
+      };
+    })
   ]
 };
