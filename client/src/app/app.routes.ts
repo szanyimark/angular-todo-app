d87a4f685d9ccca43cefde4d9b76c3370d532361
Szanyi Márk <szanyimark04@gmail.com>
2026-02-23 09:49:01 +0100

    FE: use todo list in app

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TodoList } from './components/todo-list/todo-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: TodoList,
+    title: 'Todo App'
+  }
+];
